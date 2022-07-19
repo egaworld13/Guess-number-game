a99c521763ff05ae8eccdc0e64e5ef82eab43537
Edgars Roze <edgarsroze13@gmail.com>
2022-07-19 10:34:55 +0300

    inital commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+let guess = document.querySelector('.message').textContent;
+console.log(guess);
+guess = '🎉Correct Number';
+console.log(guess);
+
+let number = document.querySelector('.number').textContent;
+number = 10;
+let number = document.querySelector('.number').textContent;
